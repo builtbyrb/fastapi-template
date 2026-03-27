962005235d3c9cb62eb74239088d1586b9820285
builtbyrb <bestdevelloper67@gmail.com>
2026-03-27 01:25:43 +0000

    fix(worker): fixed config kwargs

--- src/worker.py
+++ src/worker.py
@@ -5,6 +5,6 @@ class ProductionWorker(UvicornWorker):
     CONFIG_KWARGS = {  # noqa: RUF012
         "loop": "uvloop",
         "http": "httptools",
-        "limit-concurrency": 1000,
+        "limit_concurrency": 1000,
         "backlog": 2048,
     }
