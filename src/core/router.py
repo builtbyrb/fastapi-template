fee732b4b00012dc7067fdbbb59576415060fdf8
builtbyrb <bestdevelloper67@gmail.com>
2026-03-30 22:35:34 +0000

    fix(health-route): Fixed health route response model

--- src/core/router.py
+++ src/core/router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, status
 
+from src.core.types.schemas import HealthStatus
+
 
 health_router = APIRouter(prefix="/health", tags=["Health"])
 
@@ -9,6 +11,7 @@ health_router = APIRouter(prefix="/health", tags=["Health"])
     status_code=status.HTTP_200_OK,
     summary="Perform a Health Check",
     response_description="Return HTTP 200 if the service is healthy.",
+    response_model=HealthStatus,
 )
 async def health() -> dict[str, str]:
     return {"status": "healthy"}
--- src/core/types/schemas.py
+++ src/core/types/schemas.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from pydantic import BaseModel
 
 from src.core.dependencies import IpDep
@@ -7,3 +9,7 @@ from src.core.types.request import UserAgentHeader
 class RequestInfoInput(BaseModel):
     ip: IpDep
     user_agent: UserAgentHeader
+
+
+class HealthStatus(BaseModel):
+    status: Literal["healthy"]
