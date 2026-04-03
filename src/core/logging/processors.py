a3693cf273a625ceb0c4ce0ecb75faf925d7ddaf
builtbyrb <bestdevelloper67@gmail.com>
2026-04-03 16:12:45 +0000

    refactor(color_message): remove drop_color_message processor

--- src/core/logging/processors.py
+++
@@ -1,8 +0,0 @@
-from structlog.typing import EventDict, WrappedLogger
-
-
-def drop_color_message_key(
-    _: WrappedLogger, __: str, event_dict: EventDict
-) -> EventDict:
-    event_dict.pop("color_message")
-    return event_dict
